751ef9542758870970446c177299ac319151887b
Usama Ali <usamaa18@gmail.com>
2021-02-18 17:35:27 -0700

    added create a new user

--- exercise-tracker/server.js
+++ exercise-tracker/server.js
@@ -10,8 +10,61 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+app.use(express.urlencoded({extended: false}));
 
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+const User = mongoose.model('User', new mongoose.Schema({
+  username: {
+    type: String,
+    require: true,
+    unique: true
+  },
+  exercises: [{
+    description: {
+      type: String,
+      require: true
+    },
+    duration: {
+      type: Number,
+      require: true
+    },
+    date: Date
+  }]
+}));
+
+app.post('/api/exercise/new-user', (req, res) => {
+  console.log(req.body.username);
+  uname = req.body.username;
+  if (uname === "") {
+    res.send("Path `username` is required.")
+  }
+  else {        
+    let obj = {username: uname};
+    User.findOneAndUpdate(
+      obj,
+      {$setOnInsert: obj}, // only update if object is not found (i.e only insert)
+      {
+        fields: 'username _id', // project these fields
+        upsert: true, // insert if not found
+        new: true, // return updated object
+        runValidators: true, // validify data
+        rawResult: true // to access lastErrorObject.updatedExisting
+      },
+      (err, doc) => {
+        if (err) {res.send({error: err});}
+
+        // indicates that the username was found in the db
+        else if (doc.lastErrorObject.updatedExisting) {
+          res.send("Username already taken");
+        }
+
+        else {
+          res.send(doc);
+        }
+      }
+    );
+  }
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
